ef8235424282f548753ebc89d45ec998e3866155
Yive <6853318+Yive@users.noreply.github.com>
2024-04-16 09:40:36 -0700

    Updated Upstream (Pufferfish)
    Upstream has released updates that appear to apply and compile correctly
    
    Pufferfish Changes:
    pufferfish-gg/Pufferfish@852ea89 Updated Upstream (Paper)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.11"
+    id("io.papermc.paperweight.patcher") version "1.5.15"
 }
 
 repositories {
@@ -15,22 +15,15 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.10:fat")
-    decompiler("net.minecraftforge:forgeflower:2.0.627.2")
+    remapper("net.fabricmc:tiny-remapper:0.10.1:fat")
+    decompiler("org.vineflower:vineflower:1.11.0-20240412.144930-14")
     paperclip("io.papermc:paperclip:3.0.3")
 }
 
 subprojects {
-
     repositories {
-        mavenLocal()
         mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
         maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
         maven("https://jitpack.io")
     }
 }
@@ -38,8 +31,8 @@ subprojects {
 paperweight {
     serverProject.set(project(":pluto-server"))
 
-    remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
+    remapRepo.set("https://papermc.io/repo/repository/maven-public/")
+    decompileRepo.set("https://s01.oss.sonatype.org/content/repositories/snapshots/")
 
     useStandardUpstream("pufferfish") {
         url.set(github("pufferfish-gg", "Pufferfish"))
