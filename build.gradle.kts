55f737a80976e19f5053e747be321e3de6e01568
Yive <6853318+Yive@users.noreply.github.com>
2025-01-10 13:25:57 -0800

    Use a PR build of paperweight
    Fixes the stackoverflow issue I was experiencing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java
-    id("io.papermc.paperweight.patcher") version "2.0.0-beta.12"
+    id("io.papermc.paperweight.patcher") version "2.0.0-SNAPSHOT" // Using local build of PR-279
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
