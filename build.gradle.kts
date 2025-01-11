04c743564b8b6c8acda3d285d8a90ee4c5f2eb53
Yive <6853318+Yive@users.noreply.github.com>
2025-01-11 13:37:33 -0800

    Updated Upstream (Pufferfish)
    Upstream has released updates that appear to apply and compile correctly
    
    Pufferfish Changes:
    pufferfish-gg/Pufferfish@d619ea7 Refactor patches to be file patch based
    pufferfish-gg/Pufferfish@e9596d0 Updated Upstream (Paper)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java
-    id("io.papermc.paperweight.patcher") version "2.0.0-SNAPSHOT" // Using local build of PR-279
+    id("io.papermc.paperweight.patcher") version "2.0.0-beta.13"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
