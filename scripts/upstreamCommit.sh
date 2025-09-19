88d010fc08633e3e686a89f74743788dc65c0b20
Yive <6853318+Yive@users.noreply.github.com>
2025-09-19 13:12:08 -0700

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    PaperMC/Paper@bc2ae10 Fix default respawn dimension being null for new worlds
    PaperMC/Paper@eb42158 Use 0.0.4 release of ConcurrentUtil
    PaperMC/Paper@bb01745 1.21.9-pre2
    PaperMC/Paper@0249204 1.21.9-pre2 feature patches
    PaperMC/Paper@9360ba4 Identifying comments for MinecraftServer
    PaperMC/Paper@5d3aa53 Some diff improvements in prepare spawn task
    PaperMC/Paper@22ad451 Move some stuff to ATs
    PaperMC/Paper@0ea15c9 Drop no longer applicable ATs
    PaperMC/Paper@cf78f2a bump api-version/datapack format and add a copper torch tag
    PaperMC/Paper@60cb0df generate ClientOption enums and mark CraftBlockData impl as null marked
    PaperMC/Paper@4025289 Add pre2 to api version
    PaperMC/Paper@f2cb073 Set build channel to alpha
    PaperMC/Paper@90b29c4 Port most of the anti-xray patch
    PaperMC/Paper@4a8e88d Disable oldPaperCommit to fix CI build
    PaperMC/Paper@121ac2e Port the last Anti-XRay hunk
    PaperMC/Paper@f43641f Use ConcurrentUtil 0.0.5

--- scripts/upstreamCommit.sh
+++ scripts/upstreamCommit.sh
@@ -10,13 +10,13 @@
 set -e
 PS1="$"
 
-tetraodontidae=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/Yive/Tetraodontidae/compare/$1...$2 | jq -r '.commits[] | "Yive/Tetraodontidae@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
+paper=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PaperMC/Paper/compare/$1...$2 | jq -r '.commits[] | "PaperMC/Paper@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
 
 updated=""
 logsuffix=""
-if [ ! -z "tetraodontidae" ]; then
-    logsuffix="$logsuffix\n\nTetraodontidae Changes:\n$tetraodontidae"
-    updated="Tetraodontidae"
+if [ ! -z "paper" ]; then
+    logsuffix="$logsuffix\n\nPaper Changes:\n$paper"
+    updated="Paper"
 fi
 disclaimer="Upstream has released updates that appear to apply and compile correctly"
 
