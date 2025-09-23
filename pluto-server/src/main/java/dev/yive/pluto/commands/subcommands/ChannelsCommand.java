ce2fc5af54e1a677956942164c11616a7c8a8264
Yive <6853318+Yive@users.noreply.github.com>
2025-09-23 00:36:11 -0700

    Improve output for channels/schedulers commands
    I hope that the colours I picked are compatible with those who are colourblind.

--- pluto-server/src/main/java/dev/yive/pluto/commands/subcommands/ChannelsCommand.java
+++ pluto-server/src/main/java/dev/yive/pluto/commands/subcommands/ChannelsCommand.java
@@ -1,64 +1,76 @@
 package dev.yive.pluto.commands.subcommands;
 
 import dev.yive.pluto.commands.PlutoSubcommand;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
 public class ChannelsCommand implements PlutoSubcommand {
-    private static final List<String> COMPLETIONS = List.of("outgoing", "incoming");
     @Override
     public boolean execute(final CommandSender sender, final String subCommand, final String[] args) {
-        if (args.length != 1) {
-            sender.sendMessage(Component.text("Use /pluto channels <outgoing|incoming>", NamedTextColor.RED));
-            return false;
-        }
-
-        boolean outgoing = "outgoing".equalsIgnoreCase(args[0]);
-        Map<String, Set<String>> pluginChannels = new HashMap<>();
+        Map<String, Map<String, Set<String>>> pluginChannels = new HashMap<>();
         for (final Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
-            pluginChannels.compute(plugin.getName(), (s, strings) -> {
-                final Set<String> set = outgoing ? Bukkit.getMessenger().getOutgoingChannels(plugin) : Bukkit.getMessenger().getIncomingChannels(plugin);
-                if (set.isEmpty()) return strings;
+            pluginChannels.compute(plugin.getName(), (s, map) -> {
+                final Set<String> outgoing = Bukkit.getMessenger()
+                    .getOutgoingChannels(plugin);
+                final Set<String> incoming = Bukkit.getMessenger()
+                    .getIncomingChannels(plugin);
+                if (outgoing.isEmpty() && incoming.isEmpty()) return map;
 
-                if (strings == null) strings = new HashSet<>();
-                strings.addAll(set);
-                return strings;
+                map = map == null ? new HashMap<>() : map;
+
+                if (!outgoing.isEmpty())
+                    map.put("outgoing", outgoing);
+
+                if (!incoming.isEmpty())
+                    map.put("incoming", incoming);
+                return map;
             });
         }
 
-        final Iterator<Map.Entry<String, Set<String>>> iterator = pluginChannels.entrySet().iterator();
-        while (iterator.hasNext()) {
-            final Map.Entry<String, Set<String>> entry = iterator.next();
-            if (!entry.getValue().isEmpty()) continue;
-            iterator.remove();
-        }
+        sender.sendMessage(Component.text("Plugin Channels:", NamedTextColor.GRAY));
 
-        sender.sendMessage(Component.text(outgoing ? "Outgoing channels: " : "Incoming channels: ", NamedTextColor.GRAY).append(Component.text(pluginChannels.size(), NamedTextColor.GOLD)));
+        for (final Map.Entry<String, Map<String, Set<String>>> entry : pluginChannels.entrySet()) {
+            final Map<String, Set<String>> map = entry.getValue();
+            List<Component> components = new ArrayList<>();
 
-        for (Map.Entry<String, Set<String>> entry : pluginChannels.entrySet()) {
-            Set<String> channels = entry.getValue();
-            if (channels.isEmpty()) continue;
-
-            sender.sendMessage(Component.text(entry.getKey() + ":", NamedTextColor.YELLOW));
-            for (final String channel : channels) {
-                sender.sendMessage(Component.text("- ", NamedTextColor.GRAY).append(Component.text(channel, NamedTextColor.GOLD)));
+            final Set<String> incoming = map.getOrDefault("incoming", Set.of());
+            if (!incoming.isEmpty()) {
+                components.add(Component.text("Incoming Channels:", NamedTextColor.GRAY));
+                for (String channel : incoming) {
+                    components.add(Component.text(channel, NamedTextColor.AQUA));
+                }
             }
+
+            final Set<String> outgoing = map.getOrDefault("outgoing", Set.of());
+            if (!outgoing.isEmpty()) {
+                components.add(Component.text("Outgoing Channels:", NamedTextColor.GRAY));
+                for (String channel : outgoing) {
+                    components.add(Component.text(channel, NamedTextColor.AQUA));
+                }
+            }
+
+            if (components.isEmpty()) continue;
+
+            final Component hover = Component.join(JoinConfiguration.newlines(), components);
+
+            sender.sendMessage(
+                Component.text()
+                    .content(" - " + entry.getKey())
+                    .color(NamedTextColor.GOLD)
+                    .hoverEvent(hover)
+                    .build()
+            );
         }
 
         return true;
-    }
-
-    @Override
-    public List<String> tabComplete(final CommandSender sender, final String subCommand, final String[] args) {
-        return args.length == 1 ? COMPLETIONS : List.of();
     }
 }
--- pluto-server/src/main/java/dev/yive/pluto/commands/subcommands/SchedulersCommand.java
+++ pluto-server/src/main/java/dev/yive/pluto/commands/subcommands/SchedulersCommand.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
@@ -16,27 +17,41 @@ import org.bukkit.scheduler.BukkitWorker;
 public class SchedulersCommand implements PlutoSubcommand {
     @Override
     public boolean execute(final CommandSender sender, final String subCommand, final String[] args) {
-        Set<String> plugins = new HashSet<>();
-        Map<String, Integer> activeWorkers = new HashMap<>();
-        Map<String, Integer> pendingTasks = new HashMap<>();
-        Map<String, Integer> foliaTasks = Bukkit.getAsyncScheduler() instanceof FoliaAsyncScheduler asyncScheduler ? asyncScheduler.getTaskCount() : Map.of();
+        Map<String, Integer> activeWorkers = new HashMap<>(),
+            pendingTasks = new HashMap<>(),
+            foliaTasks = Bukkit.getAsyncScheduler() instanceof FoliaAsyncScheduler asyncScheduler ? asyncScheduler.getTaskCount() : Map.of();
+        Set<String> plugins = new HashSet<>(foliaTasks.keySet());
+
         for (final BukkitWorker activeWorker : Bukkit.getScheduler().getActiveWorkers()) {
-            plugins.add(activeWorker.getOwner().getName());
-            activeWorkers.compute(activeWorker.getOwner().getName(), (s, integer) -> integer == null ? 1 : integer + 1);
-        }
-        for (final BukkitTask pendingTask : Bukkit.getScheduler().getPendingTasks()) {
-            plugins.add(pendingTask.getOwner().getName());
-            pendingTasks.compute(pendingTask.getOwner().getName(), (s, integer) -> integer == null ? 1 : integer + 1);
+            final String plugin = activeWorker.getOwner().getName();
+            plugins.add(plugin);
+            activeWorkers.compute(plugin, (s, integer) -> integer == null ? 1 : integer + 1);
         }
 
-        sender.sendMessage(Component.text("Schedulers: ", NamedTextColor.YELLOW));
+        for (final BukkitTask pendingTask : Bukkit.getScheduler().getPendingTasks()) {
+            final String plugin = pendingTask.getOwner().getName();
+            plugins.add(plugin);
+            pendingTasks.compute(plugin, (s, integer) -> integer == null ? 1 : integer + 1);
+        }
+
+        sender.sendMessage(Component.text("Schedulers:", NamedTextColor.GRAY));
         for (final String plugin : plugins) {
+            final Component hover = Component.join(
+                JoinConfiguration.newlines(),
+                Component.text(activeWorkers.getOrDefault(plugin, 0) + " Active",
+                    NamedTextColor.AQUA),
+                Component.text(pendingTasks.getOrDefault(plugin, 0) + " Pending",
+                    NamedTextColor.AQUA),
+                Component.text(foliaTasks.getOrDefault(plugin, 0) + " Folia",
+                    NamedTextColor.AQUA)
+            );
+
             sender.sendMessage(
-                Component.text(
-                    plugin + ": " + activeWorkers.getOrDefault(plugin, 0) + " Active, " +
-                        pendingTasks.getOrDefault(plugin, 0) + " Pending, " +
-                        foliaTasks.getOrDefault(plugin, 0) + " Folia", NamedTextColor.GOLD
-                )
+                Component.text()
+                    .content(" - " + plugin)
+                    .color(NamedTextColor.GOLD)
+                    .hoverEvent(hover)
+                    .build()
             );
         }
         return true;
