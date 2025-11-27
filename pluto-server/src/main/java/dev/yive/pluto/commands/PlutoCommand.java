7a0e7ea42d200a726e3f0ef54807182522e6f9c0
Yive <6853318+Yive@users.noreply.github.com>
2025-11-26 16:42:59 -0800

    Update to 1.21.11-pre3

--- pluto-server/src/main/java/dev/yive/pluto/commands/PlutoCommand.java
+++ pluto-server/src/main/java/dev/yive/pluto/commands/PlutoCommand.java
@@ -13,7 +13,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import net.minecraft.Util;
+import net.minecraft.util.Util;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
